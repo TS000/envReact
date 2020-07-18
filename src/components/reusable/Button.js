64cffac89af6ea41c5a72ca2244dc1402eff6c08
Tyler S <ghost@Tylers-Mac-mini.local>
2020-07-18 16:12:05 -0600

    added new button

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const CoolButton = styled.button`
-  list-style: none
-  text-transform: uppercase
-  background-color: #fff
-  border: none
-  padding: 8px 60px
-  color: #585858
-  transition: all .1s
-  &:hover {
-      background: grey
-      color: #fff
-      cursor: pointer
-      border: none
-    }
-   &:active {
-      box-shadow: inset 0 0 10px #000000
-   }
-   &:focus {
-    outline: none
-   }
+const AddButton = styled.button `
+    background: none;
+    border: 2px #7D8A2E solid;
+    color: #7D8A2E;
+    border-radius: 64px;
+    height: 30px;
+    width: 30px;
+    font-weight: 700;
+    cursor: pointer;
+    float: right;
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0,0,0,0);
+    transition-duration: 0.3s;
+    transition-property: transform;
+    position: absolute;
+    top: 20%;
+    right: 5%;
+    float: ${props => (props.right ? 'right' : 'left')};
+        &:hover {
+            -webkit-transform: scale(1.1);
+            transform: scale(1.1);
+        }
 `
 
-export default class Button extends React.Component {
-  render() {
-    return (
-      <CoolButton onClick={this.props.onClick}>{this.props.title}</CoolButton>
-    )
-  }
-}
+export default ({ handleClick, direction, title }) =>
+  <AddButton direction={direction} onClick={handleClick}> {title} </AddButton>
