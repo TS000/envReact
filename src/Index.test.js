8f8752bf1b871093148b1e592b7045faf7ec2903
Tyler S <ghost@Tylers-Mac-mini.local>
2020-07-28 15:41:48 -0600

    fixed the test

--- src/Index.test.js
+++ src/Index.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import Index from './index'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
 
+configure({ adapter: new Adapter() })
 it('renders without crashing', () => {
-  shallow(<Index />)
+  shallow(<App />)
 })
