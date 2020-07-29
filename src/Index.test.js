e4edea4f18c7d6dc0071a966c7377f7232c091a5
Tyler S <ghost@Tylers-Mac-mini.local>
2020-07-28 20:15:07 -0600

    fixed test

--- src/Index.test.js
+++ src/Index.test.js
@@ -3,7 +3,7 @@ import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from './App'
 
-configure({ adapter: new Adapter() })
+
 it('renders without crashing', () => {
   shallow(<App />)
 })
