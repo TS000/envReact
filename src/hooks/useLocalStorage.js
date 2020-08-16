bc6283dbd4e1de3fb09b95299f6e1cc72f50aabd
TS000 <tylersnell@me.com>
2020-08-16 15:58:46 -0600

    local storage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+
+const getStateFromLocalStorage = (defaultValue, key) => {
+  const storage = localStorage.getItem(key)
+  if (storage) return JSON.parse(storage).value
+  return defaultValue
+}
+
+const useLocalStorage = (defaultValue, key) => {
+  const initialValue = getStateFromLocalStorage(defaultValue, key)
+  const [value, setValue] = React.useState(initialValue)
+
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }))
+  }, [value])
+
+  return [value, setValue]
+}
+
+export default useLocalStorage
+
+// const [count, setCount] = useLocalStorage(0, 'count');
