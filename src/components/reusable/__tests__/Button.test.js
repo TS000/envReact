34ab9d14e5096ff9f71e4d01278e895040253cfc
Tyler S <ghost@Tylers-Mac-mini.local>
2020-08-19 21:27:39 -0600

    added a test

---
+++ src/components/reusable/__tests__/Button.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme'
+import Button from '../Button'
+configure({ adapter: new Adapter() })
+describe('Button', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<Button />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
