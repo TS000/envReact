cb4e874a29560ecfa41bc9318405d7e3b73e208a
Tyler S <ghost@Tylers-Mac-mini.local>
2020-08-19 21:27:17 -0600

    remove old test

--- src/Index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import App from './App'
-
-
-it('renders without crashing', () => {
-  shallow(<App />)
-})
