23dc68d4b6ad2b52f61e43a221838c9e68734c4e
Tyler S <ghost@Tylers-Mac-mini.local>
2020-08-19 21:39:00 -0600

    format

--- src/components/reusable/__tests__/Button.test.js
+++ src/components/reusable/__tests__/Button.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
 import { shallow, configure } from 'enzyme'
 import Button from '../Button'
+
 configure({ adapter: new Adapter() })
 describe('Button', () => {
   it('renders correctly', () => {
