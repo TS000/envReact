2ab2ad927e8f33f80f2fac8719844cd51dfb708d
Tyler S <ghost@Tylers-Mac-mini.local>
2020-08-21 08:04:05 -0600

    now takes height and width strings

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -2,26 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 const AddButton = styled.button `
+    height: ${(props) => props.height};
+    width: ${(props) => props.width};
     background: none;
     border: 2px #7D8A2E solid;
     color: #7D8A2E;
     border-radius: 64px;
     font-weight: 700;
     cursor: pointer;
-    float: right;
     transform: perspective(1px) translateZ(0);
     box-shadow: 0 0 1px rgba(0,0,0,0);
     transition-duration: 0.3s;
     transition-property: transform;
-    position: absolute;
-    top: 20%;
-    right: 5%;
-    float: ${props => (props.right ? 'right' : 'left')};
         &:hover {
             -webkit-transform: scale(1.1);
             transform: scale(1.1);
         }
 `
 
-export default ({ handleClick, direction, title }) =>
-  <AddButton direction={direction} onClick={handleClick}> {title} </AddButton>
+export default ({ handleClick, title, height, width }) =>
+  <AddButton height={height} width={width} onClick={handleClick}> {title} </AddButton>
