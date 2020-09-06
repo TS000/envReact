342bff204846dc175c824c272829a745fdcf2f7e
TS000 <tylersnell@me.com>
2020-09-06 17:34:42 -0600

    added mdx docs support to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  stories: ['../src/**/*.stories.@(js|jsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    }
   ]
 }
