f351409cd74cb3166e52171126a964616d7b43dc
TS000 <tylersnell@me.com>
2020-09-06 21:48:05 -0600

    added a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|mdx)'],
+  stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
+    '@storybook/addon-a11y/register',
     '@storybook/addon-contexts/register',
     '@storybook/addon-backgrounds/register',
     {
