6fd6b698905f721fbde5fa0e1946407325e4be00
TS000 <tylersnell@me.com>
2020-09-06 21:54:07 -0600

    props for color

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-const AddButton = styled.button `
-    height: ${(props) => props.height};
-    width: ${(props) => props.width};
-    background: none;
-    border: 2px #7D8A2E solid;
-    color: #7D8A2E;
-    border-radius: 64px;
-    font-weight: 700;
-    cursor: pointer;
-    transform: perspective(1px) translateZ(0);
-    box-shadow: 0 0 1px rgba(0,0,0,0);
-    transition-duration: 0.3s;
-    transition-property: transform;
-        &:hover {
-            transform: scale(1.1);
-        }
+const AddButton = styled.button`
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  background: ${(props) => props.color};
+  border: ${(props) => props.border} 2px solid;
+  color: ${(props) => props.font};
+  border-radius: 64px;
+  font-weight: 700;
+  cursor: pointer;
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  transition-duration: 0.3s;
+  transition-property: transform;
+  &:hover {
+    transform: scale(1.1);
+  }
 `
 
-export default ({ handleClick, title, height, width }) =>
-  <AddButton height={height} width={width} onClick={handleClick}> {title} </AddButton>
-
+export default ({ handleClick, title, height, width, color, border, font }) => (
+  <AddButton
+    font={font}
+    color={color}
+    border={border}
+    height={height}
+    width={width}
+    onClick={handleClick}
+  >
+    {title}
+  </AddButton>
+)
