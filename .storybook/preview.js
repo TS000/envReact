880ef605b52523a43e9479a4486dbca561890364
TS000 <tylersnell@me.com>
2020-09-06 21:48:20 -0600

    added knobs and a11y

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,16 @@
-import { addParameters, addDecorator } from 'storybook/react'
+import { addParameters, addDecorator } from '@storybook/react'
 import { withContexts } from '@storybook/addon-contexts/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
 import { contexts } from './contexts'
 
 addParameters({
   backgrounds: [
-    { name: 'Default Theme', value: '#ffffff', default: true },
-    { name: 'Dark Theme ', value: '#000000' }
+    { name: 'Default Theme', value: '#fff', default: true },
+    { name: 'Dark Theme ', value: '#000' }
   ]
 })
 
+addDecorator(withKnobs)
+addDecorator(withA11y)
 addDecorator(withContexts(contexts))
